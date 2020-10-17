914497b22f7e9224036bfb70322f91d3b5b117e9
Axmouth <aggronfg@yahoo.com>
2020-10-17 22:53:13 +0300

    Fixing api domain for prod.

--- alchemy-shop-client/src/environments/environment.prod.ts
+++ alchemy-shop-client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
 };
 
-export const apiRoot = 'https://api.zhinindas.alchemy.shop.axmouth.dev';
+export const apiRoot = 'https://api.zhinindas.alchemy.shop.axmouth.dev/api/v1/';
 export const jwtWhitelist = ['zhinindas.alchemy.shop.axmouth.dev', 'api.zhinindas.alchemy.shop.axmouth.dev'];
