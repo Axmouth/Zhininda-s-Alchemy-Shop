c149427d9f2d1bfa12916cd578d3f6337bd74e71
Axmouth <aggronfg@yahoo.com>
2020-10-23 23:42:37 +0300

    Fixed db not beign reachable by api container(ops) and using wrong port to access the api container. Added a merch link to homepage too.

--- alchemy-shop-client/src/environments/environment.prod.ts
+++ alchemy-shop-client/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
 };
 
 export const apiRoot = 'https://api.zhinindas.alchemy.shop.axmouth.dev/api/v1/';
-export const apiRootServer = 'http://' + 'zhinindas-alchemy-shop-api' + ':6544/api/v1/';
+export const apiRootServer = 'http://' + 'zhinindas-alchemy-shop-api' + ':80/api/v1/';
 export const jwtWhitelist = ['zhinindas.alchemy.shop.axmouth.dev', 'api.zhinindas.alchemy.shop.axmouth.dev'];
 export const websiteUrl = 'https://zhinindas.alchemy.shop.axmouth.dev';
